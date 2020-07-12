12881476a68f4605f92b487786f2267db24d16fd
Chandershekhar Luhaniwal <csl21191@gmail.com>
2020-07-12 22:09:23 +0530

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -33,8 +33,7 @@ class App extends Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          });
-          console.log(this.state);          
+          });        
         });        
       }
       this.setState({currentUser: userAuth});
